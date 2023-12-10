11e0500a97102440f9ec3cdc3280d11151ef46ed
Leonel Meque <leomeque@gmail.com>
2023-12-10 17:54:38 +0100

    feat: add jwt

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,40 @@
+import { PassportStrategy } from '@nestjs/passport';
+import {
+  ExtractJwt,
+  Strategy,
+} from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: {
+    sub: string;
+    username: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+
+    delete user.password;
+
+    return user;
+  }
+}
